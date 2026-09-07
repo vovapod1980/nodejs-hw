61b4d0542fc1d1b154c7d50765d65593237abfda
Volodymyr Podoliako <vpodol1980@gmail.com>
2026-09-07 21:46:57 +0300

    finish

---
+++ src/models/note.js
@@ -0,0 +1,39 @@
+import { model } from 'mongoose';
+import { Schema } from 'mongoose';
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+
+    tag: {
+      type: String,
+      required: false,
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+      default: 'Todo',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+export const Note = model('Note', noteSchema);
