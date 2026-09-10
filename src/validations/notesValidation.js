5cc74ffe7814bdfa0b7b81c9b0f7659e8af544f9
Volodymyr Podoliako <vpodol1980@gmail.com>
2026-09-10 22:09:15 +0300

    rename constants

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,6 +1,6 @@
 import { Joi, Segments } from 'celebrate';
 import { isValidObjectId } from 'mongoose';
-import { TAGS } from '../contacts/tags.js';
+import { TAGS } from '../constants/tags.js';
 
 const objectIdValidator = (value, helpers) => {
   return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
