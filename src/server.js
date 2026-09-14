749c5cad359b9c6c88fa551ffcee323b798cfb70
Volodymyr Podoliako <vpodol1980@gmail.com>
2026-09-14 22:43:15 +0300

    fix error

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import { errors } from 'celebrate';
+import authRoutes from './routes/authRoutes.js';
+import cookieParser from 'cookie-parser';
 
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 
@@ -17,6 +19,8 @@ const PORT = process.env.PORT ?? 3000;
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
+app.use(authRoutes);
 
 app.use(notesRoutes);
 app.use(notFoundHandler);
