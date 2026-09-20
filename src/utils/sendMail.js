3b5c6f1d695b73d74642bcf380bffcfdb811d564
Volodymyr Podoliako <vpodol1980@gmail.com>
2026-09-20 12:50:41 +0300

    change M to m

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -9,6 +9,6 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-export const sendEMail = async (options) => {
+export const sendEmail = async (options) => {
   return await transporter.sendMail(options);
 };
